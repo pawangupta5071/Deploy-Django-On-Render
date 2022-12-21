1254dbf1c4a3df14b749b29d22a2c5a8d77efe99
pawan gupta <pawangupta5071@gmail.com>
2022-12-21 15:40:28 +0530

    fourth

--- hello_world/hello_world/settings.py
+++ hello_world/hello_world/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6h%_wm@tv29xcvcusw^v(s5o8utowkt(et8yu+zf7nl#n!os3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://hello-iampawan.onrender.com']
 
